e273fb5dcd0ca15680bcb6cbe9853e2915b60dea
Zo Obradovic <ping@obradovic.com>
2020-09-11 17:23:45 -0700

    Manage version in an external file

--- setup.py
+++ setup.py
@@ -4,13 +4,26 @@ from setuptools import setup
 
 
 def get_readme_md_contents():
-    """read the contents of your README file"""
-    with open("README.md", encoding='utf-8') as f:
-        long_description = f.read()
-        return long_description
+    """ Reads the contents of README file
+    """
+    return get_file("README.md")
+
+
+def get_version():
+    """ Returns the contents of the VERSION file
+    """
+    return get_file("VERSION")
+
+
+def get_file(filename: str) -> str:
+    """ Returns the contents of the passed-in filename
+    """
+    with open(filename, encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
-    version="0.1.5",
+    version=get_version(),
     name="google-domains",
     url="https://github.com/obradovic/google-domains",
     install_requires=[
