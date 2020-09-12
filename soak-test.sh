9163b3024a456587944542a267885767576876f0
Zo Obradovic <ping@obradovic.com>
2020-09-12 14:58:32 -0700

    DRYer

--- soak-test.sh
+++ soak-test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-alias t='TIMEFORMAT="That took %1R seconds" && time'
 
 for i in {1..200}; do
     echo
     echo "ITERATION $i"
-    TIMEFORMAT="That add took %1R seconds" && time google-domains -q add foo google.com
-    TIMEFORMAT="That del took %1R seconds" && time google-domains -q del foo
+    TIMEFORMAT="That add took %1R seconds" time google-domains -q add foo google.com
+    TIMEFORMAT="That del took %1R seconds" time google-domains -q del foo
 done
 
